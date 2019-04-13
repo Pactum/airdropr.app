c333c611ec89f167da80f3dfe8bd95be7ffe0758
Micheal Giles <mikegg21@gmail.com>
2019-04-13 01:36:29 -0500

    erc20 token input

--- airdrop/src/pages/Airdrop/components/RecipientInput.jsx
+++ airdrop/src/pages/Airdrop/components/RecipientInput.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 
 export default function RecipientInput() {
+  const [token, setToken] = useState('');
   const [address, setAddress] = useState('');
   const [amount, setAmount] = useState('');
+
+  function handleToken(e) {
+    e.preventDefault();
+    console.log(token);
+  }
 
   function handleAdd(e) {
     e.preventDefault();
@@ -13,9 +19,22 @@ export default function RecipientInput() {
     <ul className="create-list">
       <li className="create-list-item">
         <h4 className="text-align-center">
-          Create a list of airdrop recipients by adding erc20 wallet
+          Add the erc20 token address you would like to airdrop from and
+          create a list of airdrop recipients by adding erc20 wallet
           addresses with the amount of tokens you would like to send.
         </h4>
+      </li>
+      <li className="create-list-item">
+        <form className="flex" onSubmit={handleToken}>
+          <input
+            required
+            value={token}
+            onChange={e => setToken(e.target.value)}
+            placeholder="ERC20 token address"
+            type="text"
+            className="address-input ad-input"/>
+          <button type='submit'>Submit</button>
+        </form>
       </li>
       <li>
         <form className="flex" onSubmit={handleAdd}>
